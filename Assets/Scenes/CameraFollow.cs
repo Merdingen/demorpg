72135e511444da221b18d57bb4e81b51f799bd9c
Merdingen <merttartac@gmail.com>
2018-07-17 21:32:27 +0300

    game content added

---
+++ Assets/Scenes/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    private GameObject player;
+    private GameObject mainCamera;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    private void LateUpdate()
+    {
+        transform.position = player.transform.position;
+    }
+}
